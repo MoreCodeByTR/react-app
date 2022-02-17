52685b37059fcce1af069b8fc141306aad381d33
tianrui13 <tianrui13@jd.com>
2022-02-17 16:44:03 +0800

    默认选中

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { Route, Link,Redirect,useLocation } from "react-router-dom"
+import { Route, Link,Redirect,useLocation ,Switch} from "react-router-dom"
 
 import { Layout, Menu,message } from "antd"
 import "./index.css"
@@ -16,25 +16,25 @@ function Home(props) {
   return (
       <Layout>
         <Header>
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["about"]}>
-            <Menu.Item key="about">
+          <Menu theme="dark" mode="horizontal" selectedKeys={[location.pathname]}>
+            <Menu.Item key="/about">
               <Link to="/about">介绍</Link>
             </Menu.Item>
-            <Menu.Item key="adress">
+            <Menu.Item key="/adress">
               <Link to="/adress">地址</Link>
             </Menu.Item>
             <Menu.Item key="/svg">  <Link to="/svg">SVG</Link></Menu.Item>
           </Menu>
         </Header>
         <Content>
-          <>
+          <Switch>
           {route.map((item)=>{
             return( <Route key={item.path} path={item.path} render={props => <item.component {...props}  />
             }  />)
            
           })}
             <Redirect from="/*" to='/about'/>
-          </>
+          </Switch>
         </Content>
       </Layout>
   )
