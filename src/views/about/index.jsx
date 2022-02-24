5fbb30238e5b11878565b60611be71bd9499bb44
tianrui13 <tianrui13@jd.com>
2022-02-24 14:51:20 +0800

    iframe测试

--- src/views/about/index.jsx
+++ src/views/about/index.jsx
@@ -5,6 +5,7 @@ function About(){
     <h1>关于我</h1>
     <p>12233</p>
     <a href="https://v5.reactrouter.com/web/example/route-config">react-router-dom v5.0</a>
+    <iframe src="http://bjsh.wiiishang.com/csfzx.html" height={600} width={400}></iframe>
     </>
   )
 
