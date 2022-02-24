56e39dac7932e05b3027fece64ac08b3c2638516
tianrui13 <tianrui13@jd.com>
2022-02-24 15:06:00 +0800

    撤销

--- src/views/about/index.jsx
+++ src/views/about/index.jsx
@@ -5,8 +5,6 @@ function About(){
     <h1>关于我</h1>
     <p>12233</p>
     <a href="https://v5.reactrouter.com/web/example/route-config">react-router-dom v5.0</a>
-    <iframe src="http://bjsh.wiiishang.com/csfzx.html" height={600} width={400}></iframe>
-    <iframe src="https://3.cn/1rP1nG-v?&utm_source=iosapp&utm_medium=appshare&utm_campaign=t_335139774&utm_term=CopyURL&ad_od=share&utm_user=plusmember&gx=RnFiwGNcbTzdzNRP--txWzg1C9Ew_P5KcUjJ" height={600} width={400}></iframe>
     </>
   )
 
